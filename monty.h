2114b730b1f7ff8b6ff3b2cdc0999bd32d8f5a28
exclud <shammahnjomo12@gmail.com>
2023-06-22 21:15:59 +0300

    Implemented the pop opcode

--- monty.h
+++ monty.h
@@ -41,5 +41,6 @@ typedef struct instruction_s
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
