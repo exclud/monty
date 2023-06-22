238ce7a8221c4bcdc32a22634f859adcde124719
exclud <shammahnjomo12@gmail.com>
2023-06-22 21:14:07 +0300

    Implemented the Pint opcode

--- monty.h
+++ monty.h
@@ -40,5 +40,6 @@ typedef struct instruction_s
 /* Function Prototypes */
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
